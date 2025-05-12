726cf58f87bffd8c7f3410d8f0be92e8b6c96763
as-amaan-109 <official.amaansyedateeq@gmail.com>
2025-05-12 21:55:53 +0530

    commit

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const cors = require('cors');
+// const bodyParser = require('body-parser');
 
-// Create an Express app
-const app = express();
+// // Create an Express app
+// const app = express();
 
-// Middleware
-app.use(cors()); // Enables Cross-Origin Resource Sharing
-app.use(bodyParser.json()); // For parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
+// // Middleware
+// app.use(cors()); // Enables Cross-Origin Resource Sharing
+// app.use(bodyParser.json()); // For parsing application/json
+// app.use(bodyParser.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
 
-// Example route
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+// // Example route
+// app.get('/', (req, res) => {
+//   res.send('Hello, World!');
+// });
 
-const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// const PORT = 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
